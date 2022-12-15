6974b8340dcd399d9e34421f7a38aa89f68020c6
jcheng01 <83311317+jcheng01@users.noreply.github.com>
2022-12-15 13:12:45 -0500

    add new host

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ module.exports = {
             {
                 protocol: 'http',
                 hostname: 'it-363-restaurants.local',
+            },
+            {
+                protocol: 'https',
+                hostname: 'restaurants.ist363.xyz',
             }
         ]
     }
